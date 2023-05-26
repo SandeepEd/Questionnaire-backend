bf814920debd2e3e932bbb178011d9713688cdff
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-26 18:25:01 -0400

    add types

--- src/modules/user/useCases/getUser/controller.ts
+++ src/modules/user/useCases/getUser/controller.ts
@@ -1,18 +1,23 @@
 import { NextFunction } from "express";
 import { BaseController } from "../../../../shared/BaseController";
 import { GetUserUseCase } from "./useCase";
+import { Response } from "express-serve-static-core";
 
 export class GetUserController extends BaseController {
-    constructor(public getUserUseCase: GetUserUseCase) {
-        super();
-    }
+  constructor(public getUserUseCase: GetUserUseCase) {
+    super();
+  }
 
-    public async handleController(req: any, res: any, next: NextFunction): Promise<any> {
-        try{
-            const user = await this.getUserUseCase.execute(req.user.id);
-        this.handleResponse(req, res, user);
-        } catch (error) {
-            next(error)
-        }
+  public async handleController(
+    req: any,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> {
+    try {
+      const user = await this.getUserUseCase.execute(1);
+      this.handleResponse(req, res, user);
+    } catch (err) {
+      next(err);
     }
+  }
 }
