6fb3449d82f56c625af843284562bfb46342b2c8
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-27 00:44:22 -0400

    protect routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,11 @@ import express from "express";
 import { userRouter } from "./modules/user/routes";
 import { questionnaireRoute } from "./modules/questionnaire/routes";
 import { logInRouter, signUpRouter } from "./modules/auth/routes";
+import { ProtectedRoute } from "./utils/ManageRoute";
 
 const router = express.Router();
-router.use('/user', userRouter)
-router.use(`/questionnaire`, questionnaireRoute)
+router.use('/user', ProtectedRoute, userRouter)
+router.use(`/questionnaire`, ProtectedRoute, questionnaireRoute)
 router.use(`/login`, logInRouter)
 router.use(`/signup`, signUpRouter)
 
