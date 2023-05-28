7c37539c23bc02b78409bf800d58d91021d6eb50
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-27 20:44:02 -0400

    not required

--- src/database/sequelize/models/index.ts
+++ src/database/sequelize/models/index.ts
@@ -1,11 +1,9 @@
 import { UserModel } from "./user";
-import { UserResponses } from "./userResponse";
 import { QuestionsModel } from "./questions";
 import { OptionsModel } from "./options";
 import { AssignmentModel } from "./Assignment";
 
-UserResponses.associate()
 QuestionsModel.associate()
 AssignmentModel.associate()
 
-export { UserModel, UserResponses, QuestionsModel, OptionsModel, AssignmentModel }
+export { UserModel, QuestionsModel, OptionsModel, AssignmentModel }
